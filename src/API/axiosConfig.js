4395014274de9dc636d39f67a14a0a6c8e7cf8ea
Aidar <alinbekov.at@gmail.com>
2024-05-20 19:53:48 +0300

    Final?????

--- src/API/axiosConfig.js
+++ src/API/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { getAuthToken } from '../utils/auth';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8081',
+    baseURL: 'http://85.193.86.182:8081',
 });
 
 axiosInstance.interceptors.request.use(config => {
